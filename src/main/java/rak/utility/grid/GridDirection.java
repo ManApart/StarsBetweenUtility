b4ed38f28ded5918df4b7af954ea021aaa1d2143
iceburg333 <ice_burg333@yahoo.com>
2017-10-15 09:58:32 -0400

    Add cardinality and inverse directions

--- src/main/java/rak/utility/grid/GridDirection.java
+++ src/main/java/rak/utility/grid/GridDirection.java
@@ -1,21 +1,23 @@
 package rak.utility.grid;
 
 public enum GridDirection {
-	NORTH_WEST(-1,-1),
-	NORTH(0,-1),
-	NORTH_EAST(1,-1),
-	EAST(1,0),
-	SOUTH_WEST(-1,1),
-	SOUTH(0,1),
-	SOUTH_EAST(1,1),
-	WEST(-1,0);
+	NORTH_WEST(-1,-1, false),
+	NORTH(0,-1, true),
+	NORTH_EAST(1,-1, false),
+	EAST(1,0, true),
+	SOUTH_WEST(-1,1, false),
+	SOUTH(0,1, true),
+	SOUTH_EAST(1,1, false),
+	WEST(-1,0, true);
 	
 	private int offsetX;
 	private int offsetY;
+	private boolean isCardinal;
 	
-	private GridDirection(int x, int y){
+	private GridDirection(int x, int y, boolean isCardinal){
 		this.offsetX = x;
 		this.offsetY = y;
+		this.isCardinal = isCardinal;
 	}
 
 	public int getOffsetX(){
@@ -24,5 +26,31 @@ public enum GridDirection {
 	
 	public int getOffsetY(){
 		return offsetY;
+	}
+	
+	public boolean isDiagonal(){
+		return !isCardinal;
+	}
+	
+	public boolean isCardinal(){
+		return isCardinal;
+	}
+	
+	public GridDirection getInverse(){
+		int x = -1 * getOffsetX();
+		int y = -1 * getOffsetY();
+		return byValue(x, y);
+	}
+	
+	public static GridDirection byValue(int x, int y){
+		if (x > 1 || x < -1 || y > 1 || y < -1){
+			throw new IllegalArgumentException("Direction values x:" + x +", y:" + y + " are out of bounds");
+		}
+		for (GridDirection direction : GridDirection.values()){
+			if (x == direction.getOffsetX() && y == direction.getOffsetY()){
+				return direction;
+			}
+		}
+		return null;
 	}
 }
